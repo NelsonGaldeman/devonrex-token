641cca04167bce4eccd87bf71732a508cd4be2bb
Nelson Galdeman <nelsongaldeman@gmail.com>
2022-12-19 16:01:36 +0000

    feat: backup address can be set

--- contracts/DevonRex.sol
+++ contracts/DevonRex.sol
@@ -6,6 +6,9 @@ import "./interfaces/IERC20.sol";
 contract DevonRex is IERC20 {
     mapping(address => uint256) internal balances;
     mapping(address => mapping(address => uint256)) internal allowed;
+    mapping(address => address) internal backupAddresses;
+
+    event BackupAddressSet(address indexed _owner, address indexed _to);
 
     uint256 internal _totalSupply;
 
@@ -58,6 +61,15 @@ contract DevonRex is IERC20 {
         return true;
     }
 
+    /// @dev `msg.sender` sets `_to` as his backup address
+    /// @param _to The address of the account that is set as backup address
+    /// @return Always true if the call has enough gas to complete execution
+    function backup(address _to) external returns (bool) {
+        backupAddresses[msg.sender] = _to;
+        emit BackupAddressSet(msg.sender, _to);
+        return true;
+    }
+
     /// @dev Query total supply of token
     /// @return Total supply of token
     function totalSupply() external view returns (uint256) {
@@ -76,6 +88,13 @@ contract DevonRex is IERC20 {
     /// @return Amount of remaining tokens allowed to spent
     function allowance(address _owner, address _spender) external view returns (uint256) {
         return allowed[_owner][_spender];
+    }
+
+    /// @dev Query the backup address of owner
+    /// @param _owner The address from which the backup address will be retrieved
+    /// @return BackupAddress of owner
+    function backupAddressOf(address _owner) external view returns (address) {
+        return backupAddresses[_owner];
     }
 
     // @TODO: UPDATE COMMENTS NOTATION
